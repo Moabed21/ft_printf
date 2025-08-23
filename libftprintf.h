54532dae0fe44137629c9b90bde0412a530d959e
Moabed21 <moathabed21@gmail.com>
2025-08-23 14:45:40 +0300

    file creating succeed

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moabed <moabed@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 08:38:57 by moabed            #+#    #+#             */
+/*   Updated: 2025/08/22 09:01:36 by moabed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTHEADER_H
+#define LIBFTHEADER_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+int	ft_printf(const char *s,...);
+
+#endif
